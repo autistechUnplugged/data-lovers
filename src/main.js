78cb9e2334c87da6c5eb462d2c18bbb7049ed40d
vonhappatsch <vonhappatsch@gmail.com>
2019-03-05 04:44:24 -0300

    mudanças no css e html

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 const pokemonData = POKEMON["pokemon"];
+const eachPokemon = document.getElementById("eachPokemon");
+const ascOrder = document.getElementById("idOrder1to151");
+const descOrder = document.getElementById("idOrder151to1");
 const checkAtoZ = document.getElementById("alphOrderAtoZ");
 const checkZtoA = document.getElementById("alphOrderZtoA");
-const one = document.getElementById("one");
-const two = document.getElementById("two");
 
 window.onload = function () {
     goPokemons();
@@ -12,33 +13,53 @@ function getPokemons() {
     return pokemonData;
 };
 
+// tentar deixar isso aqui mais dinamico
+
 function goPokemons() {
-    let eachPokemon = document.getElementById("eachPokemon");
     eachPokemon.innerHTML = `
     ${getPokemons().map((number) => `
         <div class="pokemon-card">
-            <div class="card-title">
-                <h3 class="name-pokemon">${number["name"]}</h3>
-            </div>
             <div class="card-num">
-                <h4 class="num-pokemon">${number["num"]}<h3>
+                <h4 class="pokemon-num">${number["num"]}<h4>
+            </div>
+            <div class="card-title">
+                <h3 class="pokemon-name">${number["name"]}</h3>
             </div>
             <div class="card-img">
                 <img src="${number["img"]}" class="img-pokemon" />
             </div>
             <div class="card-type">
-                <p class="pokemon-type">${number["type"]}</p>
+                <p><b>Tipo</b></p>
+                <p class="pokemon-type">${number["type"].join(" * ")}</p>
             </div>
             <div class="card-weaknesses">
-                <p class="pokemon-weak">${number["weaknesses"]}</p>
+                <p><b>Fraqueza</b></p>
+                <p class="pokemon-weak">${number["weaknesses"].join(" * ")}</p>
             </div>
         </div>
        `).join("")}
        `
 };
 
+ascOrder.addEventListener('click', orderPokemonUp);
+descOrder.addEventListener('click', orderPokemonDown);
+
+function orderPokemonUp() {
+    pokemonData.sort(function(a,b) {
+        return a.num - b.num;
+    });
+    return goPokemons();
+};
+
+function orderPokemonDown() {
+    pokemonData.sort(function(a,b) {
+        return b.num - a.num;
+    });
+    return goPokemons();
+};
+
 function sortingPokemonsUp() {
-    let sorted = pokemonData.sort(function(a,b) {
+    pokemonData.sort(function(a,b) {
         let nameA = a.name.toUpperCase();
         let nameB = b.name.toUpperCase();
         if (nameA < nameB) {
@@ -49,24 +70,28 @@ function sortingPokemonsUp() {
         }
         return 0;
     });
-    goPokemons();
+    return goPokemons();
  }; 
 
- one.addEventListener("click", sortingPokemonsUp());
- 
-/* function sortedUp() {
-    let sorted = pokemonData.sort(function(a,b) {
-        return a.name.toLowerCase().localeCompare(b.name.toLowerCase());
+ function sortingPokemonsDown() {
+    pokemonData.sort(function(a,b) {
+        let nameA = a.name.toUpperCase();
+        let nameB = b.name.toUpperCase();
+        if (nameA < nameB) {
+            return 1;
+        }
+        if (nameA > nameB) {
+            return -1;
+        }
+        return 0;
     });
     return goPokemons();
-}
+ }; 
 
-function sortedDown() {
-    let sortedDown = pokemonData.sort(function(a,b) {
-        a.name.toLowerCase().localeCompare(b.name.toLowerCase());
-    });
-    return goPokemons();
-}
-
-one.addEventListener("click", sortedUp);
-two.addEventListener("click", sortedDown); */
+checkAtoZ.addEventListener("click", sortingPokemonsUp);
+/*
+checkZtoA.addEventListener("click", sortingPokemonsDown);
+let types = pokemonData["type"];
+console.log(types);
+*/
+checkZtoA.addEventListener("click", sortingPokemonsDown);
