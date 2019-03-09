3004b1bf1664d173ac391eb19779640a1e285ded
vonhappatsch <vonhappatsch@gmail.com>
2019-03-09 19:06:24 -0300

    nova versão

--- src/main.js
+++ src/main.js
@@ -5,8 +5,11 @@ const descOrder = document.getElementById("idOrder151to1");
 const checkAtoZ = document.getElementById("alphOrderAtoZ");
 const checkZtoA = document.getElementById("alphOrderZtoA");
 
+// const sidebar = document.getElementById("sidebar");
+
 window.onload = function () {
-    goPokemons();
+    let pokemonData = getPokemons();
+    goPokemons(pokemonData);
 };
 
 function getPokemons() {
@@ -15,94 +18,193 @@ function getPokemons() {
 
 // tentar deixar isso aqui mais dinamico
 
-function goPokemons() {
+function goPokemons(pokemonDataSource) {
     eachPokemon.innerHTML = `
-    ${getPokemons().map((number) => `
-        <div class="pokemon-card">
-            <div class="card-num">
-                <h4 class="pokemon-num">${number["num"]}<h4>
-            </div>
-            <div class="card-title">
-                <h3 class="pokemon-name">${number["name"]}</h3>
-            </div>
+    ${pokemonDataSource.map(poke => `
+        <div class="pokemon">
             <div class="card-img">
-                <img src="${number["img"]}" class="img-pokemon" />
+                <img src="${poke["img"]}" class="img-pokemon" />
             </div>
-            <div class="card-type">
-                <p><b>Tipo</b></p>
-                <p class="pokemon-type">${number["type"].join(" * ")}</p>
-            </div>
-            <div class="card-weaknesses">
-                <p><b>Fraqueza</b></p>
-                <p class="pokemon-weak">${number["weaknesses"].join(" * ")}</p>
+            <div class="pokemon-card">
+                <div class="card-num">
+                    <p class="pokemon-num">#${poke["num"]}<p>
+                </div>
+                <div class="card-title">
+                    <h3 class="pokemon-name">${poke["name"]}</h3>
+                </div>
+                <div class="card-type info">
+                    <p class="pokemon-type">${poke["type"].join(" / ")}</p>
+                    <p class="sub">Tipo</p>
+                </div>
+                <div class="card-pokemon info">
+                    <p class="pokemon-weight">${poke["weight"]}</p>
+                    <p class="sub">Peso</p>
+                </div>
+                <div class="card-pokemon info">
+                    <p class="pokemon-height">${poke["height"]}</p>
+                    <p class="sub">Altura</p>
+                </div>
+                <div class="card-weaknesses">
+                    <p class="pokemon-weaknesses">${poke["weaknesses"].join(" / ")}</p>
+                    <p class="sub">Fraqueza</p>
+                </div>
+                <div class="card-candy">
+                    <p class="pokemon-candy">${poke["candy"]}</p>
+                    <p class="sub">Candy</p>
+                </div>
+                <div class="card-candy-evo">
+                    <p class="pokemon-candy-evo">${poke["candy_count"]}</p>
+                    <p class="sub">Quantidade para evoluir</p>
+                </div>
             </div>
         </div>
-       `).join("")}
-       `
+    `).join("")}
+   `
 };
 
-ascOrder.addEventListener('click', orderPokemonUp);
-descOrder.addEventListener('click', orderPokemonDown);
+// função filtro
+
+pokeType.addEventListener('change', typeList);
+
+function typeList() {
+    let typeValue = pokeType.options[pokeType.selectedIndex].value;
+    let weaknessValue = pokeWeakness.options[pokeWeakness.selectedIndex].value;
+
+    if (weaknessValue != "") {
+        if (typeValue == '') {
+            console.log("Returning weaknessList from Type");
+            return weaknessList();
+        }
+        
+        let pokeType = pokemonData.filter(monster => monster.type.includes(typeValue));
+        let pokeWeaknessAndType = pokeType.filter(monster => monster.weaknesses.includes(weaknessValue));
+
+        if (pokeWeaknessAndType == '') {
+            console.log("Vazio");
+            return eachPokemon.innerHTML = "<h1>Sem Pokémom</h1>";
+        }
+        console.log("Returning FILTERED Weakness And Type from Type");
+        return goPokemons(pokeWeaknessAndType);
+    } else {
+        if (typeValue == '') {
+            console.log("Returns PokemonData from Type");
+            return goPokemons(pokemonData);
+        }
+        console.log("Returns PokemonData FILTERED from Type");
+        return goPokemons(pokemonData.filter(monster => monster.type.includes(typeValue)));
+    }
+
+};
+
+pokeWeakness.addEventListener('change', weaknessList);
+
+function weaknessList() {
+    let weaknessValue = pokeWeakness.options[pokeWeakness.selectedIndex].value;
+    let typeValue = pokeType.options[pokeType.selectedIndex].value;
+
+    if (typeValue != "") {
+
+        if (weaknessValue == '') {
+            console.log("Returning typeList from weaknessList");
+            return typeList();
+        }
+
+        let pokeWeakness = pokemonData.filter(monster => monster.weaknesses.includes(weaknessValue));
+        let pokeTypeAndWeakness = pokeWeakness.filter(monster => monster.type.includes(typeValue));
+        if (pokeTypeAndWeakness == '') {
+            console.log("Vazio");
+            return eachPokemon.innerHTML = "<h1>Sem Pokémom</h1>";
+        }
+        console.log("Returning FILTERED Type and Weakness from Weakness");
+        return goPokemons(pokeTypeAndWeakness);
+    } else {
+        if (weaknessValue == '') {
+            console.log("Returns PokemonData from Weakness");
+            return goPokemons(pokemonData);
+        }
+        console.log("Returns PokemonData FILTERED from Weakness");
+        return goPokemons(pokemonData.filter(monster => monster.weaknesses.includes(weaknessValue)));
+    }
+    
+};
+
+
+function makeInvisible() {
+    const navbar = document.getElementById("navbar");
+    const pokedex = document.getElementById("pokedex");
+    const initial = document.getElementById("initial");
+    const another = document.getElementById("another");
+    pokedex.style.display = "none";
+    initial.style.display = "none";
+    navbar.style.display = "flex";
+    eachPokemon.style.display = "block";
+    another.style.display = "block";
+}
+
+
+function reset(){
+    document.getElementById("pokeType").selectedIndex = 0;
+    document.getElementById("pokeWeakness").selectedIndex = 0;
+
+
+    }
 
 function orderPokemonUp() {
-    pokemonData.sort(function(a,b) {
-        return a.num - b.num;
-    });
-    return goPokemons();
+    reset();
+    return goPokemons(
+        pokemonData.sort(function (a, b) {
+            return a.num - b.num;
+        }));
 };
+
 
 function orderPokemonDown() {
-    pokemonData.sort(function(a,b) {
-        return b.num - a.num;
-    });
-    return goPokemons();
+    reset();
+    return goPokemons(
+        pokemonData.sort(function (a, b) {
+            return b.num - a.num;
+        }));
 };
 
+
 function sortingPokemonsUp() {
-    pokemonData.sort(function(a,b) {
-        let nameA = a.name.toUpperCase();
-        let nameB = b.name.toUpperCase();
-        if (nameA < nameB) {
-            return -1;
-        }
-        if (nameA > nameB) {
-            return 1;
-        }
-        return 0;
-    });
-    return goPokemons();
- }; 
+    reset();
+    return goPokemons(
+        pokemonData.sort(function (a, b) {
+            let nameA = a.name.toUpperCase();
+            let nameB = b.name.toUpperCase();
+            if (nameA < nameB) {
+                return -1;
+            }
+            if (nameA > nameB) {
+                return 1;
+            }
+            return 0;
+        })
+    );
+};
 
- function sortingPokemonsDown() {
-    pokemonData.sort(function(a,b) {
-        let nameA = a.name.toUpperCase();
-        let nameB = b.name.toUpperCase();
-        if (nameA < nameB) {
-            return 1;
-        }
-        if (nameA > nameB) {
-            return -1;
-        }
-        return 0;
-    });
-    return goPokemons();
- }; 
 
+function sortingPokemonsDown() {
+    reset();
+    return goPokemons(
+        pokemonData.sort(function (a, b) {
+            let nameA = a.name.toUpperCase();
+            let nameB = b.name.toUpperCase();
+            if (nameA < nameB) {
+                return 1;
+            }
+            if (nameA > nameB) {
+                return -1;
+            }
+            return 0;
+        })
+    );
+};
+
+
+sidebar.addEventListener("click", makeInvisible);
 checkAtoZ.addEventListener("click", sortingPokemonsUp);
-/*
 checkZtoA.addEventListener("click", sortingPokemonsDown);
-
-let types = pokemonData["type"];
-console.log(types);
-*/
-checkZtoA.addEventListener("click", sortingPokemonsDown);
-
-// função de filtro para tipo e fraqueza
-
-// um load more para os pokemons
-
-// cartãozinho com as informações
-
-// e depois as funções de filtro posteriores
-
-// gráficos
+ascOrder.addEventListener('click', orderPokemonUp);
+descOrder.addEventListener('click', orderPokemonDown);
